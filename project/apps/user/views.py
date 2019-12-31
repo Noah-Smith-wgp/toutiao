b58c6492f9fa71b1df60c8b81042bec115b22765
Noah-Smith-wgp <18211672297@163.com>
2019-12-31 10:23:09 +0800

    user蓝图创建类视图

--- project/apps/user/views.py
+++ project/apps/user/views.py
@@ -1,6 +1,20 @@
 from project.apps.user import user_buleprint
+from flask_restful import Api, Resource
 
 
-@user_buleprint.route('/login')
-def login():
-    return {'msg': 'OK'}
+# 使用api接管蓝图
+user_api = Api(user_buleprint)
+
+
+# @user_buleprint.route('/login')
+# def login():
+#     return {'msg': 'OK'}
+
+class LoginView(Resource):
+
+    def get(self):
+
+        return {'msg': 'OK kakakak'}
+
+
+user_api.add_resource(LoginView, '/login')
