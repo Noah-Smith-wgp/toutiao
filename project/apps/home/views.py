ff8f3c93e1cabc2af6c99465425b93ca5339afdb
Noah-Smith-wgp <18211672297@163.com>
2019-12-31 10:26:58 +0800

    home蓝图创建类视图

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,10 +1,24 @@
 from flask import session
+from flask_restful import Api, Resource
 
 from project.apps.home import home_buleprint
 
 
-@home_buleprint.route('/')
-def index():
+home_api = Api(home_buleprint)
 
-    session['id'] = 'abc'
-    return '--index--!!!'
+
+# @home_buleprint.route('/')
+# def index():
+#
+#     session['id'] = 'abc'
+#     return '--index--!!!'
+
+
+class IndexView(Resource):
+
+    def get(self):
+
+        return {'msg': 'index'}
+
+
+home_api.add_resource(IndexView, '/')
