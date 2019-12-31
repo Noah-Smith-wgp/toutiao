0541dacab6ce52237ba9e6ab38a7df74f7087448
Noah-Smith-wgp <18211672297@163.com>
2019-12-31 09:10:54 +0800

    抽取配置文件settings

--- manage.py
+++ manage.py
@@ -5,33 +5,14 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from settings import Config
 
 app = Flask(__name__)
-
-
-class Config(object):
-    DEBUG = True
-
-    # 定义MySQL配置
-    SQLALCHEMY_DATABASE_URI = 'mysql://toutiao:mysql@122.51.161.120:3306/toutiao'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 定义Redis配置
-    REDIS_HOST = '49.232.164.126'
-    REDIS_PORT = 6379
-
-    # flask_session的配置信息
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
-
 
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-manager = Manager(app)
+
 
 # 创建一个redis实例
 redis_store = redis.Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
@@ -39,6 +20,8 @@ redis_store = redis.Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 Session(app)
 
 CORS(app)
+
+manager = Manager(app)
 
 Migrate(app=app, db=db)
 
---
+++ settings.py
@@ -0,0 +1,20 @@
+import redis
+
+
+class Config(object):
+    DEBUG = True
+
+    # 定义MySQL配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://toutiao:mysql@122.51.161.120:3306/toutiao'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 定义Redis配置
+    REDIS_HOST = '49.232.164.126'
+    REDIS_PORT = 6379
+
+    # flask_session的配置信息
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
+    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
