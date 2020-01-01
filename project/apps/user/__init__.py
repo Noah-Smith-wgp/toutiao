250de99af7ed0961cdb96a660087990ea65981dd
Noah-Smith-wgp <18211672297@163.com>
2020-01-01 17:24:17 +0800

    实现发送短信视图

--- project/apps/user/__init__.py
+++ project/apps/user/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-user_buleprint = Blueprint('user', __name__)
+user_buleprint = Blueprint('user', __name__, url_prefix='/app/v1_0')
 
 from . import views
--- project/apps/user/views.py
+++ project/apps/user/views.py
@@ -6,9 +6,45 @@ from flask_restful import Api, Resource
 user_api = Api(user_buleprint)
 
 
-# @user_buleprint.route('/login')
-# def login():
-#     return {'msg': 'OK'}
+from flask import make_response, current_app
+from flask_restful.utils import PY3
+from json import dumps
+
+
+@user_api.representation('application/json')
+def output_json(data, code, headers=None):
+    """Makes a Flask response with a JSON encoded body"""
+
+    if 'message' not in data:
+        data = {
+            'message': 'OK',
+            'data': data
+        }
+
+    settings = current_app.config.get('RESTFUL_JSON', {})
+
+    # If we're in debug mode, and the indent is not set, we set it to a
+    # reasonable value here.  Note that this won't override any existing value
+    # that was set.  We also set the "sort_keys" value.
+    if current_app.debug:
+        settings.setdefault('indent', 4)
+        settings.setdefault('sort_keys', not PY3)
+
+    # always end the json dumps with a new line
+    # see https://github.com/mitsuhiko/flask/pull/1262
+    dumped = dumps(data, **settings) + "\n"
+
+    resp = make_response(dumped, code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+class SmsCodeResource(Resource):
+
+    def get(self, mobile):
+
+        return {'mobile': mobile}
+
 
 class LoginView(Resource):
 
@@ -17,4 +53,5 @@ class LoginView(Resource):
         return {'msg': 'OK kakakak'}
 
 
+user_api.add_resource(SmsCodeResource, '/sms/codes/<mobile>/')
 user_api.add_resource(LoginView, '/login')
